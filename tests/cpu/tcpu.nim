b063ccd1ec59c185d88bbbe75c19b87783fd510f
Derek Dai <daiderek@gmail.com>
2022-07-26 16:44:50 +0800

    add unit test for SUB

--- tests/cpu/tcpu.nim
+++ tests/cpu/tcpu.nim
@@ -398,3 +398,32 @@ block:
   assert cpu.r(A) == 0x10
   assert cpu.f == {Flag.H}
 
+block:
+  let ops = gbasm:
+    SUB A,A                 # 1
+    LD A,1                  # 2
+    LD B,1
+    SUB A,B
+    SUB A,B                 # 3
+    LD A,0x10               # 4
+    SUB A,B
+    XOR A,A                 # 5
+    LD B,0x10
+    SUB A,B
+  let cpu = newCpu(ops)
+  cpu.step                  # 1
+  assert cpu.r(A) == 0
+  assert cpu.f == {Z, N}
+  cpu.step 3                # 2
+  assert cpu.r(A) == 0
+  assert cpu.f == {Z, N}
+  cpu.step                  # 3
+  assert cpu.r(A) == 0xff
+  assert cpu.f == {Flag.C, Flag.H, N}
+  cpu.step 2                # 4
+  assert cpu.r(A) == 0xf
+  assert cpu.f == {Flag.H, N}
+  cpu.step 3                # 5
+  assert cpu.r(A) == 0xf0
+  assert cpu.f == {Flag.C, N}
+
