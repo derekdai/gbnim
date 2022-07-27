37fe550f81baf5229990cdb09af77273fb6be3b9
Derek Dai <daiderek@gmail.com>
2022-07-27 14:55:20 +0800

    add tests for BIT, SET, RES, SWAP opcodes

--- tests/cpu/tcpu.nim
+++ tests/cpu/tcpu.nim
@@ -37,15 +37,15 @@ v RET
 v CPL
 v SCF
 v CCF
+v SWAP
+v SET
+v BIT
+v RES
   DAA
   RST
   SLA
   SRA
-  SWAP
   SRL
-  BIT
-  RES
-  SET
 ]#
 
 proc newCpu(opcodes: seq[byte]): Sm83 =
@@ -856,3 +856,111 @@ block:
   assert cpu.f == {Flag.C}
   cpu.step                # 4
   assert cpu.f == {Flag.C}
+
+block:
+  let ops = gbasm:
+    SWAP A                # 1
+    LD D,0xf0             # 2
+    SWAP D
+    LD HL,0xc000          # 3
+    LD (HL),0xab
+    SWAP (HL)
+  let cpu = newCpu(ops)
+  cpu.step                # 1
+  assert cpu.r(A) == 0
+  assert cpu.f == {Z}
+  cpu.step 2              # 2
+  assert cpu.r(D) == 0x0f
+  assert cpu.f == {}
+  cpu.step 3              # 3
+  assert cpu[0xc000] == 0xba
+  assert cpu.f == {}
+
+block:
+  let ops = gbasm:
+    SET 0,B               # 1
+    SET 7,A               # 2
+    LD HL,0xc000          # 3
+    SET 3,(HL)
+    SET 4,(HL)
+    LD D,0xff             # 4
+    SET 0,D
+    SET 7,D
+  let cpu = newCpu(ops)
+  cpu.f = {Z, N, C, H}
+  cpu.step                # 1
+  assert cpu.r(B) == 0b0000_0001
+  assert cpu.f == {Z, N, C, H}
+  cpu.f = {Z, N}
+  cpu.step                # 2
+  assert cpu.r(A) == 0b1000_0000
+  assert cpu.f == {Z, N}
+  cpu.f = {}
+  cpu.step 3              # 3
+  assert cpu[0xc000] == 0b0001_1000
+  assert cpu.f == {}
+  cpu.step 2              # 4
+  assert cpu.r(D) == 0xff
+  assert cpu.f == {}
+
+block:
+  let ops = gbasm:
+    RES 0,B               # 1
+    RES 7,A               # 2
+    LD D,0xff             # 3
+    RES 0,D
+    RES 7,D
+    LD HL,0xc000          # 4
+    LD (HL),0xff
+    RES 3,(HL)
+    RES 4,(HL)
+  let cpu = newCpu(ops)
+  cpu.f = {Z, N, C, H}
+  cpu.step                # 1
+  assert cpu.r(B) == 0
+  assert cpu.f == {Z, N, C, H}
+  cpu.f = {Z, N}
+  cpu.step                # 2
+  assert cpu.r(A) == 0
+  assert cpu.f == {Z, N}
+  cpu.f = {}              # 3
+  cpu.step 3
+  assert cpu.r(D) == 0b0111_1110
+  assert cpu.f == {}
+  cpu.step 4              # 4
+  assert cpu[0xc000] == 0b1110_0111
+  assert cpu.f == {}
+
+block:
+  let ops = gbasm:
+    BIT 0,B               # 1
+    BIT 7,A               # 2
+    LD E,0b1110_1111      # 3
+    BIT 0,E
+    BIT 7,E
+    BIT 4,E
+    LD HL,0xc000          # 4
+    SET 3,(HL)
+    BIT 3,(HL)
+    RES 3,(HL)
+    BIT 3,(HL)
+  let cpu = newCpu(ops)
+  cpu.f{C} = true
+  cpu.step                # 1
+  assert cpu.f == {Z, H, C}
+  assert cpu.r(B) == 0
+  cpu.f{C} = false
+  cpu.step                # 2
+  assert cpu.f == {Z, H}
+  assert cpu.r(A) == 0
+  cpu.f{C} = false
+  cpu.step 2              # 3
+  assert cpu.f == {Flag.H}
+  cpu.step
+  assert cpu.f == {Flag.H}
+  cpu.step
+  assert cpu.f == {Z, H}
+  cpu.step 3              # 4
+  assert cpu.f == {Flag.H}
+  cpu.step 2
+  assert cpu.f == {Z, H}
