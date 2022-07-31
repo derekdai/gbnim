74123f234d3f06595d5d2b6d644d39cf2de498eb
Derek Dai <daiderek@gmail.com>
2022-07-31 22:38:58 +0800

    fine tune `newCpu` to allow only minimal set of features

---
+++ tests/common.nim
@@ -0,0 +1,43 @@
+import std/[strformat, strutils]
+import cpu, memory, io, timer
+export cpu, memory
+
+type
+  FeatureFlag* = enum
+    ffWRam0
+    ffHRam
+    ffTimer
+    ffInterrupt
+  FeatureFlags* = set[FeatureFlag]
+
+proc newCpu*(opcodes: seq[byte]; flags: FeatureFlags = {ffHram, ffWRam0}): Sm83 =
+  result = newSm83()
+  result.memCtrl = newMemoryCtrl()
+  let bootrom = newRom(BootRom, opcodes)
+  result.memCtrl.map(bootrom)
+  if ffWRam0 in flags:
+    let wram = newRam(WRAM0)
+    result.memCtrl.map(wram)
+  if ffHRam in flags:
+    let hram = newRam(HRAM)
+    result.memCtrl.map(hram)
+
+  let iomem = newIoMemory(result)
+  if ffInterrupt in flags:
+    iomem.setHandler(IoIf, loadIf, storeIf)
+  if ffTimer in flags:
+    discard newTimer(iomem)
+  result.memCtrl.map(iomem)
+
+proc step*(self: Sm83; n: int) =
+  for i in 0..<n:
+    self.step()
+
+proc dump*[T: SomeInteger](self: openArray[T]): string =
+  result = "["
+  for v in self:
+    result.add &"0x{v:x}, "
+  if result.endsWith(", "):
+    result.setLen(result.len - 2)
+  result.add "]"
+
--- tests/cpu/tinterrupt.nim
+++ tests/cpu/tinterrupt.nim
@@ -4,7 +4,7 @@ import common
 block:
   let ops = gbasm:
     NOP
-  let cpu = newCpu(ops)
+  let cpu = newCpu(ops, {ffHram, ffWRam0, ffInterrupt})
   assert not cpu.ime
   cpu.step
   assert cpu.pc == 1
@@ -22,19 +22,19 @@ block:
     NOP
     LD C,0x0f               # 5
     LD (0xff00+C),A
-  let cpu = newCpu(ops)
+  let cpu = newCpu(ops, {ffWRam0, ffInterrupt})
   cpu.step                  # 1
   assert cpu.ime == true
   cpu.step                  # 2
   assert cpu.ime == false
-  cpu.step 2                # 3
+  cpu.step 2                # 3, 啟用 interrupt
   assert cpu.ime == true
   assert cpu.pc == 4
-  cpu.step 5                # 4
+  cpu.step 5                # 4, 寫 0b000_0001 到 IE 啟用 VBlank interrupt
   assert cpu.ime == true
   assert cpu[0xffff] == 1
   assert cpu.pc == 11
-  cpu.step 3                # 5
+  cpu.step 3                # 5, 寫 0xb0000_0001 到 IF, 觸發 VBlank interrupt
   assert cpu.ime == false
   assert cpu[0xffff] == 1
   assert cpu[0xff0f] == 0
